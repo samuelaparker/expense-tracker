9629467748347de5ab410c12b3eb00d4efcc9598
Samuel Parker <samuel.a.parker00@gmail.com>
2024-05-08 16:20:06 -0400

    create and add components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,26 @@
-<template>My App</template>
+<template>
+  <HeaderItem />
+  <div class="container">
+    <BalanceItem />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import HeaderItem from './components/HeaderItem.vue'
+import BalanceItem from './components/BalanceItem.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+export default {
+  components: {
+    HeaderItem,
+    BalanceItem,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
+  }
+}
+</script>

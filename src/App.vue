fb4f50e622001dca1ca9461f6f92987d30d20919
Samuel Parker <samuel.a.parker00@gmail.com>
2024-05-09 10:53:42 -0400

    refactor App component to use composition API

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,10 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import HeaderItem from './components/HeaderItem.vue'
 import BalanceItem from './components/BalanceItem.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
-export default {
-  components: {
-    HeaderItem,
-    BalanceItem,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction
-  }
-}
 </script>

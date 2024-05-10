955b9b2c406288570ee7380520fbe51ea2561153
Samuel Parker <samuel.a.parker00@gmail.com>
2024-05-10 10:34:34 -0400

    change input label

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -2,8 +2,8 @@
   <h3>Add new transaction</h3>
   <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
-      <label for="text">Text</label>
-      <input type="text" id="text" v-model="text" placeholder="Enter text..." />
+      <label for="text">Label</label>
+      <input type="text" id="text" v-model="text" placeholder="Enter label..." />
     </div>
     <div class="form-control">
       <label for="amount"
